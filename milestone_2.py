a7671d520bd78e00d6a21651c6feb2bba07186ec
Pratik Patil <pratikpatil814@gmail.com>
2023-08-04 13:55:04 +0100

    remove milestone_2.py

--- milestone_2.py
+++
@@ -1,9 +0,0 @@
-import random
-word_list = ["apple","banana","grapes","watermelon","kiwi"]
-choice = random.choice(word_list)
-word = choice
-guess = str(input("enter a single letter "))
-if not len(guess) > 1 :
-    print("Good guess!")
-else :
-    print("Oops! That is not a valid input.")
